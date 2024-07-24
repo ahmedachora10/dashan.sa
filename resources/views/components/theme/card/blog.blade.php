41ed53112ff7a609162ccfc500d43d1103c56b20
ahmedachora10 <gjr8195@gmail.com>
2024-07-24 19:54:29 +0100

    update blog style

--- resources/views/components/theme/card/blog.blade.php
+++ resources/views/components/theme/card/blog.blade.php
@@ -2,16 +2,16 @@
 <div class="tp-blog-3-item mb-30 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".7s">
     <div class="tp-blog-3-thumb p-relative fix">
         <img src="{{$image}}" alt="" height="300px">
-        <div class="tp-blog-3-icon">
+        {{-- <div class="tp-blog-3-icon">
             <a href="blog-details.html"><i class="flaticon-next"></i></a>
-        </div>
+        </div> --}}
     </div>
-    <div class="tp-blog-3-content text-center z-index">
+    <div class="tp-blog-3-content text-center z-index"style="height: 300px !important">
         {{-- <div class="tp-blog-3-meta pb-25">
             <span><i class="fa-light fa-circle-user"></i>By thempure</span>
             <span><i class="flaticon-tag"></i>Repair</span>
         </div> --}}
-        <h4 class="tp-blog-3-title"><a href="{{$link}}">{{ $title }}</a></h4>
+        <h4 class="tp-blog-3-title">{{ $title }}</h4>
         <p>{{ $description }}</p>
     </div>
 </div>
