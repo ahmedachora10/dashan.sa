7b299273bbea8e985bffad4b6535bbd31f6bd02d
ahmedachora10 <gjr8195@gmail.com>
2024-07-26 04:14:09 +0100

    add reviews sectino

--- resources/views/components/theme/sections/our-clients.blade.php
+++ resources/views/components/theme/sections/our-clients.blade.php
@@ -1,15 +1,15 @@
 <!-- brand area start -->
-<div class="tp-brand-area tp-brand-3-bg p-relative pt-140 pb-140 fix">
+<div class="tp-brand-area tp-brand-3-bg p-relative pt-60 pb-60 fix">
     <div class="tp-brand-3-shape">
         <img src="{{asset('theme/img/brand/home-3/brand-3-bg.png')}}" alt="">
     </div>
     <div class="container">
-        <div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-2">
+        <div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-2 align-items-center">
             @foreach ($clients as $item)
             <div class="col-xl">
                 <a href="{{$item->link}}" target="_blank" class="tp-brand-item">
-                    <img width="120" height="108" class="tp-brand-item-img home-3" src="{{$item->thumbnail}}" alt="">
-                    <img width="120" height="108" class="tp-brand-item-hover" src="{{$item->thumbnail}}" alt="">
+                    <img width="60" height="60" class="tp-brand-item-img home-3" src="{{$item->thumbnail}}" alt="">
+                    <img width="60" height="60" class="tp-brand-item-hover" src="{{$item->thumbnail}}" alt="">
                 </a>
             </div>
             @endforeach
