d22457129b5ad7b0e052444f201db8dc416d6712
ahmedachora10 <gjr8195@gmail.com>
2024-07-27 00:42:55 +0100

    update loader style

--- resources/views/components/theme/loading.blade.php
+++ resources/views/components/theme/loading.blade.php
@@ -8,12 +8,12 @@
                     <div class="tp-preloader-circle">
                         <svg width="190" height="190" viewBox="0 0 380 380" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
-                            <circle stroke="#D9D9D9" cx="190" cy="190" r="180" stroke-width="6" stroke-linecap="round">
+                            <circle stroke="rgb(190 178 155)" cx="190" cy="190" r="180" stroke-width="6" stroke-linecap="round">
                             </circle>
                             <circle stroke="red" cx="190" cy="190" r="180" stroke-width="6" stroke-linecap="round">
                             </circle>
                         </svg>
-                        <img src="assets/img/logo/preloder.png" alt="">
+                        <img width="100" src="{{asset(setting('logo'))}}" alt="">
                     </div>
                 </div>
             </div>
