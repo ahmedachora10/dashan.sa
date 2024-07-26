274326a0a98c00c26592727800f8c2c843f9e411
ahmedachora10 <gjr8195@gmail.com>
2024-07-26 01:55:44 +0100

    update

--- resources/views/components/theme/sections/services.blade.php
+++ resources/views/components/theme/sections/services.blade.php
@@ -2,6 +2,11 @@
 <section class="tp-service-3-area p-relative pt-120 pb-90">
     <div class="container">
         <div class="row">
+            <div class="col-lg-12">
+                <div class="tp-blog-3-title-wrapper text-center mb-65">
+                    <x-theme.headline key="service" />
+                </div>
+            </div>
             @foreach ($services as $service)
             <div class="col-xl-3 col-sm-6">
                 <x-theme.card.service
