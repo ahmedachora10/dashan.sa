1778316a8316881e1ddeda64d8ad9f775b68b0f6
ahmedachora10 <gjr8195@gmail.com>
2024-07-26 01:09:06 +0100

    update

--- resources/views/components/theme/headline.blade.php
+++ resources/views/components/theme/headline.blade.php
@@ -1,8 +1,8 @@
 @props(['key' => '',])
 
 @if($key !== '')
-<span class="tp-section-title-pre">{{ headline($key)->subTitle }}</span>
 <h3 class="tp-section-title">{{headline($key)->title}}</h3>
+<span class="tp-section-title-pre">{{ headline($key)->subTitle }}</span>
 @else
 <span></span>
 @endif
