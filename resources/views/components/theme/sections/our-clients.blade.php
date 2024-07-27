9c415ff818a23e03ae78af3cac47d6e301f0cf33
ahmedachora10 <gjr8195@gmail.com>
2024-07-27 01:34:29 +0100

    update brand style

--- resources/views/components/theme/sections/our-clients.blade.php
+++ resources/views/components/theme/sections/our-clients.blade.php
@@ -4,15 +4,17 @@
         <img src="{{asset('theme/img/brand/home-3/brand-3-bg.png')}}" alt="">
     </div>
     <div class="container">
-        <div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-2 align-items-center">
-            @foreach ($clients as $item)
-            <div class="col-xl">
-                <a href="{{$item->link}}" target="_blank" class="tp-brand-item">
-                    <img width="60" height="60" class="tp-brand-item-img home-3" src="{{$item->thumbnail}}" alt="">
-                    <img width="60" height="60" class="tp-brand-item-hover" src="{{$item->thumbnail}}" alt="">
-                </a>
+        <div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-2 align-items-center tp-brand-active swiper-container fix">
+            <div class="swiper-wrapper">
+                @foreach ($clients as $item)
+                <div class="swiper-slide text-center">
+                    <a href="{{$item->link}}" target="_blank" class="tp-brand-item">
+                        <img width="60" height="60" class="tp-brand-item-img home-3" src="{{$item->thumbnail}}" alt="">
+                        <img width="60" height="60" class="tp-brand-item-hover" src="{{$item->thumbnail}}" alt="">
+                    </a>
+                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
 </div>
