fcb6a8f825c3bb96ccf1cf88420d4a0431804f07
ahmedachora10 <gjr8195@gmail.com>
2024-07-27 03:37:33 +0100

    fix some issues

--- resources/views/components/theme/sections/our-clients.blade.php
+++ resources/views/components/theme/sections/our-clients.blade.php
@@ -3,19 +3,17 @@
     <div class="tp-brand-3-shape">
         <img src="{{asset('theme/img/brand/home-3/brand-3-bg.png')}}" alt="">
     </div>
-    <div class="container">
-        <div>
-            <div class="tp-brand-active swiper-container fix">
-                <div class="swiper-wrapper align-items-center">
-                    @foreach ($clients as $item)
-                    <div class="swiper-slide text-center">
-                        <a href="{{$item->link}}" target="_blank" class="tp-brand-item">
-                            <img class="tp-brand-item-img home-3" src="{{$item->thumbnail}}" alt="">
-                            <img class="tp-brand-item-hover" src="{{$item->thumbnail}}" alt="">
-                        </a>
-                    </div>
-                    @endforeach
+    <div class="container px-0">
+        <div class="tp-brand-active swiper-container fix">
+            <div class="swiper-wrapper align-items-center">
+                @foreach ($clients as $item)
+                <div class="swiper-slide text-center">
+                    <a href="{{$item->link}}" target="_blank" class="tp-brand-item">
+                        <img class="tp-brand-item-img home-3" src="{{$item->thumbnail}}" alt="">
+                        <img class="tp-brand-item-hover" src="{{$item->thumbnail}}" alt="">
+                    </a>
                 </div>
+                @endforeach
             </div>
         </div>
     </div>
