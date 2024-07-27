bd385c6d5ff93e62a1df2684c9ace44b529230f2
ahmedachora10 <gjr8195@gmail.com>
2024-07-27 17:00:52 +0100

    update why us section design

--- resources/views/components/theme/sections/our-clients.blade.php
+++ resources/views/components/theme/sections/our-clients.blade.php
@@ -1,5 +1,5 @@
 <!-- brand area start -->
-<div class="tp-brand-area tp-brand-3-bg p-relative pt-60 pb-60 fix" id="clients" style="background-image: url({{asset('theme/img/brand/home-3/brand-bg.jpg')}}) !important; background-size:cover">
+<div class="tp-brand-area tp-brand-3-bg p-relative pt-60 pb-60 fix" id="clients" style="background-image: url({{asset('theme/img/brand/home-3/brand-bg.jpg')}}) !important; background-size:cover;background-position: center;">
     <div class="tp-brand-3-shape">
         {{-- <img src="{{asset('theme/img/brand/home-3/brand-bg.jpg')}}" alt=""> --}}
     </div>
