1858cfd7e396b026a5ebb511f6902c495e1d01e0
ahmedachora10 <gjr8195@gmail.com>
2024-08-08 14:32:56 +0100

    update

--- app/Services/InstagramService.php
+++ app/Services/InstagramService.php
@@ -27,7 +27,7 @@ class InstagramService {
         if(!$userProfile?->profile) {
             return [];
         }
-        return $userProfile->feed();
+        return $userProfile;
         // ->filter(fn($feed) => strtolower($feed?->type) == 'image')
         // ->map(fn($feed) => $feed->image);
     }
