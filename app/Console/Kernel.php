f53e848f5fd959cf5e651105874b1c3f5b283fd4
ahmedachora10 <gjr8195@gmail.com>
2024-08-10 13:39:53 +0100

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('instagram-feed:refresh-tokens')->monthly();
-        $schedule->command('instagram-feed:refresh')->everyMinute();
+        $schedule->command('instagram-feed:refresh')->daily();
     }
 
     /**
