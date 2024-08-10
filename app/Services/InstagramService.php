764524082e7c192cb7ecc4db14a64cb84758eb43
ahmedachora10 <gjr8195@gmail.com>
2024-08-10 13:29:46 +0100

    update

--- app/Services/InstagramService.php
+++ app/Services/InstagramService.php
@@ -5,14 +5,7 @@ namespace App\Services;
 use Dymantic\InstagramFeed\InstagramFeed;
 
 class InstagramService {
-
-    private $profile;
-
     const PROFILE_NAME = 'dashan';
-
-    public function __construct() {
-        $this->profile = \Dymantic\InstagramFeed\Profile::for(self::PROFILE_NAME);
-    }
 
     public static function auth() {
         return \Dymantic\InstagramFeed\Profile::new(self::PROFILE_NAME)->getInstagramAuthUrl();
@@ -29,6 +22,10 @@ class InstagramService {
         foreach($feeds as $index => $feed) {
             if (!strtolower($feed?->type) == 'image')
                 continue;
+
+            if ($index > 5)
+                break;
+
             $images[$index]['image'] = $feed->thumbnail_url != '' ? $feed->thumbnail_url : $feed->url;
             $images[$index]['permalink'] = $feed->permalink;
         }
