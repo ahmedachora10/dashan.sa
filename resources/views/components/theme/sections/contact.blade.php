7d4f5611791f45f279115b6e530f2eb7520becf0
ahmedachora10 <gjr8195@gmail.com>
2024-08-12 23:59:17 +0100

    update footer icon color

--- resources/views/components/theme/sections/contact.blade.php
+++ resources/views/components/theme/sections/contact.blade.php
@@ -2,7 +2,7 @@
 <section class="tp-contact-7-area p-relative pt-120">
     <div class="tp-contact-7-bg" data-background="{{asset('theme/img/contact/contact-bg.jpg')}}"></div>
     <div class="tp-contact-7-shape">
-    <img src="{{asset('theme/img/contact/contact.jpg')}}" alt="">
+    <img src="{{asset('theme/img/contact/contact.jpg')}}" width="419" height="693" alt="">
     </div>
     <div class="container">
     <div class="row">
