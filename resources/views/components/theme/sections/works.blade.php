58fecec134d068eda3f423037855f3bae681d6ca
ahmedachora10 <gjr8195@gmail.com>
2025-06-15 17:56:24 +0100

    fix design issue

--- resources/views/components/theme/sections/works.blade.php
+++ resources/views/components/theme/sections/works.blade.php
@@ -121,7 +121,7 @@
                                 <div class="col-lg-6 wow fadeInUp" data-wow-delay=".4s">
                                     <div class="feature-item">
                                         <div class="icon">
-                                            <i class="fas"> <img src="{{ asset($whyUsIcon->thumbnail) }}" class="lazy" alt="{{ $whyUsIcon->thumbnail }}"></i>
+                                            <i class="fas"> <img src="{{ asset($whyUsIcon->thumbnail) }}" class="lazy"></i>
                                         </div>
                                         <div class="cont">
                                             <h5 style="margin: bottom 10px;" class="custom-sub-title">{{ headline($whyUsIcon->title_en)->title }}</h5>
